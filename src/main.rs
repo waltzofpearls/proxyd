3ea236d299e742468ee991fdce7c233e26ff1466
Rollie Ma <rollie.ma@gmail.com>
2019-02-25 10:19:39 -0800

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::io::Result;
+use std::net::{TcpListener, TcpStream};
+
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+
+    for client in listener.incoming() {
+        handle(client?)?;
+    }
+    Ok(())
+}
+
+fn handle(mut client: TcpStream) -> Result<()> {
+    let mut server = TcpStream::connect("127.0.0.1:9000")?;
+    server.write(&[1])?;
+    server.read(&mut [0; 128])?;
+    // client.write(&[1])?;
+    // client.read(&mut [0; 128])?;
+    client.write_all(b"HTTP/1.0 200 OK\r\n\r\n").unwrap();
+    Ok(())
 }
