53950be90c3a1b2d00b3cea5061e6abf19585a31
Rollie Ma <rollie.ma@gmail.com>
2019-03-07 13:28:11 -0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::io::Result;
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
 
 fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
@@ -12,11 +13,26 @@ fn main() -> Result<()> {
 }
 
 fn handle(mut client: TcpStream) -> Result<()> {
-    let mut request = vec![0; 1024];
-    client.read(&mut request)?;
-
+    client.set_read_timeout(Some(Duration::from_millis(100)))?;
     let mut server = TcpStream::connect("127.0.0.1:9000")?;
-    server.write_all(&request)?;
+    loop {
+        let mut buf = [0];
+        let _ = match client.read(&mut buf) {
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::WouldBlock => {
+                    break;
+                }
+                _ => return Err(e),
+            },
+            Ok(bytes) => {
+                if bytes == 0 {
+                    break;
+                }
+                server.write(&mut buf)?;
+            }
+        };
+    }
+
     server.shutdown(std::net::Shutdown::Write)?;
 
     let mut response = Vec::new();
