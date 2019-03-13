7aacb8ddd060403006b7fbdac6f5051b6f016565
Rollie Ma <rollie.ma@gmail.com>
2019-03-13 10:43:44 -0700

    multithread: add concurrency support

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 use std::io::prelude::*;
 use std::io::Result;
 use std::net::{TcpListener, TcpStream};
-use std::time::Duration;
+use std::{thread, time};
 
 fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
 
     for client in listener.incoming() {
-        handle(client?)?;
+        let client = client.unwrap();
+
+        thread::spawn(|| match handle(client) {
+            Ok(_) => println!(""),
+            Err(e) => println!("{}", e),
+        });
     }
+    drop(listener);
     Ok(())
 }
 
 fn handle(mut client: TcpStream) -> Result<()> {
-    client.set_read_timeout(Some(Duration::from_millis(100)))?;
+    client.set_read_timeout(Some(time::Duration::from_millis(100)))?;
     let mut server = TcpStream::connect("127.0.0.1:9000")?;
     loop {
         let mut buf = [0];
